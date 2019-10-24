eb3dfeba7fafa3c514f0fc36bf94d36124336755
alpo.8 <142857@myself.com>
2019-10-24 16:27:12 +0300

    static interface loaders

--- Interfaces/IHeader.cs
+++ Interfaces/IHeader.cs
@@ -1,17 +1,33 @@
+using System.Runtime.CompilerServices;
 using RomTool.Data;
 
 namespace RomTool.Interfaces
 {
     public interface IHeader
     {
-        static UInt24 fullSize;
-        int FullSize => fullSize;
-        int Size => GetType().StructLayoutAttribute.Size;
+        static UInt24 _fullSize;
+
+        int FullSize 
+            => _fullSize;
+
+        int Size 
+            => GetType().StructLayoutAttribute.Size;
+
+        static void Load(UInt24 fullSize) 
+            => _fullSize = fullSize;
     }
 
     public interface ITypedHeader : IHeader
     {
-        static byte type;
-        BlockType Type => (BlockType)type;
+        static byte _type;
+
+        BlockType Type 
+            => (BlockType)_type;
+
+        static void Load(UInt24 fullSize, byte type)
+        {
+            Load(fullSize);
+            _type = type;
+        }
     }
 }
